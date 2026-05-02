439bfef111a33f1ff5d422c12d90c8be757d59a1
Khush Shah <kshah17121@gmail.com>
2026-05-02 12:34:14 +0530

    feat: updated Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.scss';
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
